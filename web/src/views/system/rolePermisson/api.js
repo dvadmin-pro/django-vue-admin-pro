49df7cb2e3312c781a8843694e710d4fcddca892
李强 <1206709430@qq.com>
2021-07-18 01:20:51 +0000

    !10 权限管理功能完善 * 重构: 修改首页 * 功能变化(dvadmin): 权限管理功能完善 * 配置变化(dvadmin): 配置数据库密码变化

--- web/src/views/system/rolePermisson/api.js
+++ web/src/views/system/rolePermisson/api.js
@@ -2,12 +2,12 @@
  * @创建文件时间: 2021-06-01 22:41:21
  * @Auther: 猿小天
  * @最后修改人: 猿小天
- * @最后修改时间: 2021-06-05 15:55:29
+ * @最后修改时间: 2021-07-17 19:16:06
  * 联系Qq:1638245306
  * @文件介绍: 角色管理接口
  */
 import { request } from '@/api/service'
-export function GetList (query) {
+export function GetList(query) {
   return request({
     url: '/api/system/role/',
     method: 'get',
@@ -17,14 +17,14 @@ export function GetList (query) {
   })
 }
 
-export function GetObj (obj) {
+export function GetObj(obj) {
   return request({
     url: '/api/system/role/' + obj.id + '/',
     method: 'get'
   })
 }
 
-export function createObj (obj) {
+export function createObj(obj) {
   return request({
     url: '/api/system/role/',
     method: 'post',
@@ -32,14 +32,14 @@ export function createObj (obj) {
   })
 }
 
-export function UpdateObj (obj) {
+export function UpdateObj(obj) {
   return request({
     url: '/api/system/role/' + obj.id + '/',
     method: 'put',
     data: obj
   })
 }
-export function DelObj (id) {
+export function DelObj(id) {
   return request({
     url: '/api/system/role/' + id + '/',
     method: 'delete',
@@ -48,7 +48,7 @@ export function DelObj (id) {
 }
 
 // 通过角色id,获取菜单数据
-export function GetMenuData (obj) {
+export function GetMenuData(obj) {
   return request({
     url: '/api/system/role_id_to_menu/' + obj.id + '/',
     method: 'get',
@@ -57,6 +57,21 @@ export function GetMenuData (obj) {
     // 将列表数据转换为树形数据
     // let data = XEUtils.toArrayTree(res.data.data, { parentKey: 'parent' })
     const data = res.data.data
+    return data
+  })
+}
+
+// 获取部门数据,用于角色授权
+export function GetDeptData() {
+  return request({
+    url: '/api/system/dept/',
+    method: 'get',
+    params: {}
+  }).then(res => {
+    // 将列表数据转换为树形数据
+
+    const data = res.data.data
+
     return data
   })
 }
