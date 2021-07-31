1f069841345f93fbf0896bc290dc9fd9d99cb6e6
猿小天 <1638245306@qq.com>
2021-07-31 10:59:23 +0800

    配置变化(dvadmin):
    配置swagger的登录

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -264,6 +264,32 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True
 }
 
+# ====================================#
+# ****************swagger************#
+#====================================#
+SWAGGER_SETTINGS = {
+    # 基础样式
+    'SECURITY_DEFINITIONS': {
+    "basic":{
+        'type': 'basic'
+        }
+    },
+    # 如果需要登录才能够查看接口文档, 登录的链接使用restframework自带的.
+    'LOGIN_URL': 'rest_framework:login',
+    'LOGOUT_URL': 'rest_framework:logout',
+    # 'DOC_EXPANSION': None,
+    # 'SHOW_REQUEST_HEADERS':True,
+    # 'USE_SESSION_AUTH': True,
+    # 'DOC_EXPANSION': 'list',
+    # 接口文档中方法列表以首字母升序排列
+    'APIS_SORTER': 'alpha',
+    # 如果支持json提交, 则接口文档中包含json输入框
+    'JSON_EDITOR': True,
+    # 方法列表字母排序
+    'OPERATIONS_SORTER': 'alpha',
+    'VALIDATOR_URL': None,
+}
+
 # ================================================= #
 # ******************** 其他配置 ******************** #
 # ================================================= #
