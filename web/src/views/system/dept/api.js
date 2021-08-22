b7e9c04bf02ab69e097b03e136863d8aa95de96f
猿小天 <1638245306@qq.com>
2021-08-22 23:09:25 +0800

    修复BUG(dvadmin): 修复权限管理中部门获取的bug

--- web/src/views/system/dept/api.js
+++ web/src/views/system/dept/api.js
@@ -2,7 +2,7 @@
  * @创建文件时间: 2021-06-01 22:41:21
  * @Auther: 猿小天
  * @最后修改人: 猿小天
- * @最后修改时间: 2021-07-29 19:22:36
+ * @最后修改时间: 2021-08-22 23:07:58
  * 联系Qq:1638245306
  * @文件介绍: 部门管理接口
  */
@@ -13,7 +13,7 @@ export const urlPrefix = '/api/system/dept/'
 /**
  * 列表查询
  */
-export function GetList (query) {
+export function GetList(query) {
   query.limit = 999
   return request({
     url: urlPrefix,
@@ -21,14 +21,14 @@ export function GetList (query) {
     params: query
   }).then(res => {
     // 将列表数据转换为树形数据
-    res.data.data = XEUtils.toArrayTree(res.data.data, { parentKey: 'parent', strict: false })
+    res = XEUtils.toArrayTree(res.data.data, { parentKey: 'parent', strict: false })
     return res
   })
 }
 /**
  * 新增
  */
-export function createObj (obj) {
+export function createObj(obj) {
   return request({
     url: urlPrefix,
     method: 'post',
@@ -39,7 +39,7 @@ export function createObj (obj) {
 /**
  * 修改
  */
-export function UpdateObj (obj) {
+export function UpdateObj(obj) {
   return request({
     url: urlPrefix + obj.id + '/',
     method: 'put',
@@ -49,7 +49,7 @@ export function UpdateObj (obj) {
 /**
  * 删除
  */
-export function DelObj (id) {
+export function DelObj(id) {
   return request({
     url: urlPrefix + id + '/',
     method: 'delete',
