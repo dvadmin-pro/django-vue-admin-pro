2b9ff02f19e7dd3104fb32479f0442ebfab2b24f
猿小天 <1638245306@qq.com>
2021-08-23 23:06:39 +0800

    新功能(dvadmin): 新增定时任务的详情查看

--- web/src/views/system/menu/crud.js
+++ web/src/views/system/menu/crud.js
@@ -24,33 +24,33 @@ export const crudOptions = (vm) => {
       view: {
         thin: true,
         text: '',
-        disabled() {
+        disabled () {
           return !vm.hasPermissions('Retrieve')
         }
       },
       edit: {
         thin: true,
         text: '',
-        disabled() {
+        disabled () {
           return !vm.hasPermissions('Update')
         }
       },
       remove: {
         thin: true,
         text: '',
-        disabled() {
+        disabled () {
           return !vm.hasPermissions('Delete')
         }
       },
       width: 230,
       custom: [{
-        show(index, row) {
+        show (index, row) {
           if (row.web_path) {
             return true
           }
           return false
         },
-        disabled() {
+        disabled () {
           return !vm.hasPermissions('Update')
         },
         text: ' 按钮配置',
@@ -232,7 +232,7 @@ export const crudOptions = (vm) => {
             placeholder: '请输入路由地址'
           },
           helper: {
-            render(h) {
+            render (h) {
               return (< el-alert title="浏览器中url的地址,请以/开头" type="warning" />
               )
             }
@@ -250,7 +250,7 @@ export const crudOptions = (vm) => {
             placeholder: '请输入组件名称'
           },
           helper: {
-            render(h) {
+            render (h) {
               return (< el-alert title="xx.vue文件中的name" type="warning" />
               )
             }
@@ -274,7 +274,7 @@ export const crudOptions = (vm) => {
             placeholder: '请输入组件地址'
           },
           helper: {
-            render(h) {
+            render (h) {
               return (< el-alert title="src/views下的文件夹地址" type="warning" />
               )
             }
@@ -325,7 +325,7 @@ export const crudOptions = (vm) => {
             placeholder: '请选择是否缓存'
           },
           helper: {
-            render(h) {
+            render (h) {
               return (< el-alert title="是否开启页面缓存,需要组件名称和xx.vue页面的name一致" type="warning" />
               )
             }
@@ -349,7 +349,7 @@ export const crudOptions = (vm) => {
             placeholder: '请选择侧边可见'
           },
           helper: {
-            render(h) {
+            render (h) {
               return (< el-alert title="是否显示在侧边菜单中" type="warning" />
               )
             }
