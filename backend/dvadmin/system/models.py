95e50b56f4837e693a29c043bc8ae96a94affdde
李强 <1206709430@qq.com>
2021-09-07 22:22:04 +0800

    功能变化: 图片model优化

--- backend/dvadmin/system/models.py
+++ backend/dvadmin/system/models.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -211,9 +212,24 @@ class OperationLog(CoreModel):
         ordering = ('-create_datetime',)
 
 
+def media_img_name(instance, filename):
+    h = instance.md5sum
+    basename, ext = os.path.splitext(filename)
+    return os.path.join('media/imgs', h[0:1], h[1:2], h + ext.lower())
+
+
 class ImgList(CoreModel):
     name = models.CharField(max_length=50, null=True, blank=True, verbose_name="名称", help_text="名称")
-    url = models.ImageField(upload_to='media/imgs/%Y%m%d/')
+    url = models.ImageField(upload_to=media_img_name)
+    md5sum = models.CharField(max_length=36, blank=True, verbose_name="文件md5", help_text="文件md5")
+
+    def save(self, *args, **kwargs):
+        if not self.md5sum:  # file is new
+            md5 = hashlib.md5()
+            for chunk in self.url.chunks():
+                md5.update(chunk)
+            self.md5sum = md5.hexdigest()
+        super(ImgList, self).save(*args, **kwargs)
 
     class Meta:
         db_table = table_prefix + 'img_list'
@@ -222,14 +238,27 @@ class ImgList(CoreModel):
         ordering = ('-create_datetime',)
 
 
+def media_file_name(instance, filename):
+    h = instance.md5sum
+    basename, ext = os.path.splitext(filename)
+    return os.path.join('media/files', h[0:1], h[1:2], h + ext.lower())
+
+
 class FileList(CoreModel):
     name = models.CharField(max_length=50, null=True, blank=True, verbose_name="名称", help_text="名称")
-    url = models.FileField(upload_to='media/files/%Y%m%d/')
+    url = models.FileField(upload_to=media_file_name)
+    md5sum = models.CharField(max_length=36, blank=True, verbose_name="文件md5", help_text="文件md5")
+
+    def save(self, *args, **kwargs):
+        if not self.md5sum:  # file is new
+            md5 = hashlib.md5()
+            for chunk in self.url.chunks():
+                md5.update(chunk)
+            self.md5sum = md5.hexdigest()
+        super(FileList, self).save(*args, **kwargs)
 
     class Meta:
         db_table = table_prefix + 'file_list'
         verbose_name = '文件管理'
         verbose_name_plural = verbose_name
         ordering = ('-create_datetime',)
-
-
