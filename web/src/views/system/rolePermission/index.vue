bcdaba213c6b6dfb3e479698cbc5dcb831a478db
李强 <1206709430@qq.com>
2021-09-26 23:41:12 +0800

    修复BUG: 无效路由导致系统无法进入，添加校验路由是否有效

--- web/src/views/system/rolePermission/index.vue
+++ web/src/views/system/rolePermission/index.vue
@@ -147,18 +147,18 @@
 </template>
 
 <script>
-import * as api from "./api";
-import * as deptApi from "../dept/api";
-import XEUtils from "xe-utils";
-import Vue from "vue";
-import SplitPane from "vue-splitpane";
+import * as api from './api'
+import * as deptApi from '../dept/api'
+import XEUtils from 'xe-utils'
+import Vue from 'vue'
+import SplitPane from 'vue-splitpane'
 
-Vue.component("SplitPane", SplitPane);
+Vue.component('SplitPane', SplitPane)
 export default {
-  name: "rolePermission",
-  data() {
+  name: 'rolePermission',
+  data () {
     return {
-      filterText: "",
+      filterText: '',
       data: [],
       roleObj: {
         name: null,
@@ -172,160 +172,160 @@ export default {
       dataScopeOptions: [
         {
           value: 0,
-          label: "仅本人数据权限"
+          label: '仅本人数据权限'
         },
         {
           value: 1,
-          label: "本部门数据权限"
+          label: '本部门数据权限'
         },
         {
           value: 2,
-          label: "本部门及以下数据权限"
+          label: '本部门及以下数据权限'
         },
         {
           value: 3,
-          label: "全部数据权限"
+          label: '全部数据权限'
         },
         {
           value: 4,
-          label: "自定数据权限"
+          label: '自定数据权限'
         }
       ],
-      dataAuthorizationTips: "授权用户可操作的数据范围",
-      menuAuthorizationTips: "授权用户在菜单中可操作的范围"
-    };
+      dataAuthorizationTips: '授权用户可操作的数据范围',
+      menuAuthorizationTips: '授权用户在菜单中可操作的范围'
+    }
   },
   watch: {
-    filterText(val) {
-      this.$refs.tree.filter(val);
+    filterText (val) {
+      this.$refs.tree.filter(val)
     }
   },
   methods: {
-    filterNode(value, data) {
-      if (!value) return true;
-      return data.label.indexOf(value) !== -1;
+    filterNode (value, data) {
+      if (!value) return true
+      return data.label.indexOf(value) !== -1
     },
-    getCrudOptions() {
+    getCrudOptions () {
       // eslint-disable-next-line no-undef
-      return crudOptions(this);
+      return crudOptions(this)
     },
-    pageRequest(query) {
+    pageRequest (query) {
       return api.GetList(query).then(res => {
         res.map((value, index) => {
-          value.node_id = index;
-        });
-        this.data = res;
+          value.node_id = index
+        })
+        this.data = res
         this.$nextTick().then(() => {
-          this.initNode();
-        });
-      });
+          this.initNode()
+        })
+      })
     },
-    initNode() {
+    initNode () {
       if (this.$route.params.id && this.$refs.tree) {
         this.data.map(value => {
           if (this.$route.params.id === value.id) {
-            this.node_id = value.node_id;
+            this.node_id = value.node_id
           }
-        });
-        const node = this.$refs.tree.getNode(this.node_id);
-        this.$refs.tree.setCurrentKey(node);
-        this.nodeClick(node.data, node);
+        })
+        const node = this.$refs.tree.getNode(this.node_id)
+        this.$refs.tree.setCurrentKey(node)
+        this.nodeClick(node.data, node)
       }
     },
-    addRequest(row) {
-      return api.createObj(row);
+    addRequest (row) {
+      return api.createObj(row)
     },
-    updateRequest(row) {
-      return api.UpdateObj(row);
+    updateRequest (row) {
+      return api.UpdateObj(row)
     },
-    delRequest(row) {
-      return api.DelObj(row.id);
+    delRequest (row) {
+      return api.DelObj(row.id)
     },
     // 获取部门数据
-    getDeptData() {
+    getDeptData () {
       deptApi.GetList({ status: 1 }).then(ret => {
-        this.deptOptions = ret.data.data;
-      });
+        this.deptOptions = ret.data.data
+      })
     },
     // 获取菜单数据
-    getMenuData(data) {
+    getMenuData (data) {
       api.GetMenuData(data).then(res => {
         res.forEach(x => {
           // 根据当前角色的permission,对menuPermisson进行勾选处理
           x.menuPermission.forEach(a => {
             if (data.permission.indexOf(a.id) > -1) {
-              this.$set(a, "checked", true);
+              this.$set(a, 'checked', true)
             } else {
-              this.$set(a, "checked", false);
+              this.$set(a, 'checked', false)
             }
-          });
-        });
+          })
+        })
         // 将菜单列表转换为树形列表
         this.menuOptions = XEUtils.toArrayTree(res, {
-          parentKey: "parent",
+          parentKey: 'parent',
           strict: true
-        });
-      });
+        })
+      })
     },
     // 角色树被点击
-    nodeClick(data, node, self) {
-      this.roleObj = data;
-      this.getDeptData();
-      this.getMenuData(data);
-      this.menuCheckedKeys = data.menu; // 加载已勾选的菜单
-      this.deptCheckedKeys = data.dept;
+    nodeClick (data, node, self) {
+      this.roleObj = data
+      this.getDeptData()
+      this.getMenuData(data)
+      this.menuCheckedKeys = data.menu // 加载已勾选的菜单
+      this.deptCheckedKeys = data.dept
     },
     // 所有勾选菜单节点数据
-    getMenuAllCheckedKeys() {
+    getMenuAllCheckedKeys () {
       // 目前被选中的菜单节点
-      const checkedKeys = this.$refs.menuTree.getCheckedKeys();
+      const checkedKeys = this.$refs.menuTree.getCheckedKeys()
       // 半选中的菜单节点
-      const halfCheckedKeys = this.$refs.menuTree.getHalfCheckedKeys();
-      checkedKeys.unshift.apply(checkedKeys, halfCheckedKeys);
-      return checkedKeys;
+      const halfCheckedKeys = this.$refs.menuTree.getHalfCheckedKeys()
+      checkedKeys.unshift.apply(checkedKeys, halfCheckedKeys)
+      return checkedKeys
     },
     // 所有自定义权限时,勾选的部门节点数据
-    getDeptAllCheckedKeys() {
+    getDeptAllCheckedKeys () {
       // 目前被选中的部门节点
-      const checkedKeys = this.$refs.dept.getCheckedKeys();
+      const checkedKeys = this.$refs.dept.getCheckedKeys()
       // 半选中的部门节点
-      const halfCheckedKeys = this.$refs.dept.getHalfCheckedKeys();
-      checkedKeys.unshift.apply(checkedKeys, halfCheckedKeys);
-      return checkedKeys;
+      const halfCheckedKeys = this.$refs.dept.getHalfCheckedKeys()
+      checkedKeys.unshift.apply(checkedKeys, halfCheckedKeys)
+      return checkedKeys
     },
     // 提交修改
-    submitPermisson() {
-      this.roleObj.menu = this.getMenuAllCheckedKeys(); // 获取选中的菜单
-      this.roleObj.dept = this.getDeptAllCheckedKeys(); // 获取选中的部门
-      const menuData = XEUtils.toTreeArray(this.menuOptions);
-      const permissionData = [];
+    submitPermisson () {
+      this.roleObj.menu = this.getMenuAllCheckedKeys() // 获取选中的菜单
+      this.roleObj.dept = this.getDeptAllCheckedKeys() // 获取选中的部门
+      const menuData = XEUtils.toTreeArray(this.menuOptions)
+      const permissionData = []
       menuData.forEach(x => {
         const checkedPermission = x.menuPermission.filter(f => {
-          return f.checked;
-        });
+          return f.checked
+        })
 
         if (checkedPermission.length > 0) {
           for (const item of checkedPermission) {
-            permissionData.push(item.id);
+            permissionData.push(item.id)
           }
         }
-      });
-      this.roleObj.permission = permissionData;
+      })
+      this.roleObj.permission = permissionData
       return this.updateRequest(this.roleObj).then(res => {
-        this.$message.success("更新成功");
-      });
+        this.$message.success('更新成功')
+      })
     },
     /** 选择角色权限范围触发 */
-    dataScopeSelectChange(value) {
+    dataScopeSelectChange (value) {
       if (value !== 4) {
         // this.$refs.dept.setCheckedKeys([]);
       }
     }
   },
-  created() {
-    this.pageRequest();
+  created () {
+    this.pageRequest()
   }
-};
+}
 </script>
 
 <style lang="scss">
