cb85cf268d1483ffb10405fd31ab61a5539fb048
李强 <1206709430@qq.com>
2021-09-28 00:04:57 +0800

    功能变化: 添加 whitenoise 静态页面功能

--- backend/application/asgi.py
+++ backend/application/asgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 
 application = get_asgi_application()
-application = DjangoWhiteNoise(application)
