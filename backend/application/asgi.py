066eec320738622383bd3a83dc654e57ef378a89
李强 <1206709430@qq.com>
2021-09-27 22:21:23 +0800

    新功能: 添加线上静态文件转发模块

--- backend/application/asgi.py
+++ backend/application/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 
 application = get_asgi_application()
+application = DjangoWhiteNoise(application)
